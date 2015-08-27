95a06eea37d459e2d9695744f244b920581333ee
Adlan Khalidi <adlan@dezeek.com>
2015-08-27 08:36:30 +0800

    New background and more pages
    New background, past paper page, updated summary chapters.

--- js/my-app.js
+++ js/my-app.js
@@ -9,40 +9,115 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
+ 
+/*=== Default standalone ===*/
+var myPhotoBrowserStandalone = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ]
 });
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
+//Open photo browser on click
+$$('.pb-standalone').on('click', function () {
+    myPhotoBrowserStandalone.open();
+});
+ 
+/*=== Popup ===*/
+var myPhotoBrowserPopup = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ],
+    type: 'popup'
+});
+$$('.pb-popup').on('click', function () {
+    myPhotoBrowserPopup.open();
+});
+ 
+/*=== As Page ===*/
+var myPhotoBrowserPage = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ],
+    type: 'page',
+    backLinkText: 'Back'
+});
+$$('.pb-page').on('click', function () {
+    myPhotoBrowserPage.open();
+});
+ 
+/*=== Standalone Dark ===*/
+var myPhotoBrowserDark = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ],
+    theme: 'dark'
+});
+$$('.pb-standalone-dark').on('click', function () {
+    myPhotoBrowserDark.open();
+});
+ 
+/*=== Popup Dark ===*/
+var myPhotoBrowserPopupDark = myApp.photoBrowser({
+    photos : [
+        'http://lorempixel.com/1024/1024/sports/1/',
+        'http://lorempixel.com/1024/1024/sports/2/',
+        'http://lorempixel.com/1024/1024/sports/3/',
+    ],
+    theme: 'dark',
+    type: 'popup'
+});
+$$('.pb-popup-dark').on('click', function () {
+    myPhotoBrowserPopupDark.open();
+}); 
+ 
+/*=== With Captions ===*/
+var myPhotoBrowserPopupDark = myApp.photoBrowser({
+    photos : [
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/1/',
+            caption: 'Caption 1 Text'
+        },
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/2/',
+            caption: 'Second Caption Text'
+        },
+        // This one without caption
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/3/',
+        },
+    ],
+    theme: 'dark',
+    type: 'standalone'
+});
+$$('.pb-standalone-captions').on('click', function () {
+    myPhotoBrowserPopupDark.open();
+});
+ 
+/*=== With Video ===*/
+var myPhotoBrowserPopupDark = myApp.photoBrowser({
+    photos : [
+        {
+            html: '<iframe src="//www.youtube.com/embed/lmc21V-zBq0?list=PLpj0FBQgLGEr3mtZ5BTwtmSwF1dkPrPRM" frameborder="0" allowfullscreen></iframe>',
+            caption: 'Woodkid - Run Boy Run (Official HD Video)'
+        },
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/2/',
+            caption: 'Second Caption Text'
+        },
+        {
+            url: 'http://lorempixel.com/1024/1024/sports/3/',
+        },
+    ],
+    theme: 'dark',
+    type: 'standalone'
+});
+$$('.pb-standalone-video').on('click', function () {
+    myPhotoBrowserPopupDark.open();
+});
